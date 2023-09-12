9f91815f4bf51575c7df04d9c5711c274686147e
avedrenn <avedrenn@42.student.fr>
2023-09-12 14:08:09 +0200

    Default world not working :planet:

--- Criterion_tests/tests/test_world.c
+++ Criterion_tests/tests/test_world.c
@@ -12,7 +12,7 @@ Test(World, empty_world)
 	cr_expect(w.objs == NULL);
 }
 
-/* Test(World, default)
+Test(World, default)
 {
 	t_world	w;
 	t_light	l;
@@ -27,12 +27,15 @@ Test(World, empty_world)
 	s1->material.specular = 0.2;
 	s2 = void_obj();
 	s2->transform = scaling(0.5, 0.5, 0.5);
-	 cr_expect(sub_tuple(w.light.position, l.position) < EPSILON);
-	cr_expect(sub_tuple(w.light.intensity, l.intensity) < EPSILON);
-	cr_expect(sub_tuple(w.objs->content.transform, s1->transform) < EPSILON);
-	 cr_expect(sub_tuple(w.objs->content->material.color, s1->material.color) < EPSILON);
-	cr_expect(sub_tuple(w.objs->content->material.diffuse, s1->material.diffuse) < EPSILON);
-	cr_expect(sub_tuple(w.objs->content->material.specular, s1->material.specular) < EPSILON);
-	cr_expect(sub_tuple(w.objs->next->content->transform, s2->transform) < EPSILON);
+	cr_expect(w.light->position.x == l.position.x);
+	cr_assert(false, "%f should be %f", w.light->position.x,  l.position.x);
+	cr_expect(w.light->intensity.x == l.intensity.x);
+	cr_assert(false, "%f should be %f", w.light->intensity.x,  l.intensity.x);
+	/* cr_expect(matrix_cmp_4(w->objs->(t_obj *)content->transform, s1->transform) == 0);
+	cr_expect(matrix_cmp_4(w->objs->(t_obj *)content->material.color, s1->material.color) == 0);
+	cr_expect(w.objs->(t_obj *)content->material.diffuse == s1->material.diffuse);
+	cr_expect(w.objs->(t_obj *)content->material.specular == s1->material.specular);
+	cr_expect(matrix_cmp_4(w.objs->next->(t_obj *)content->transform, s2->transform) == 0);
 
-} */
+  */
+}
